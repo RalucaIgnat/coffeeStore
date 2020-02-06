da90d8c96770dbffb5b3c7162c4c6d5d08df45f2
unknown <ignatraluca93@gmail.com>
2020-02-06 19:04:10 +0200

    First Commit

---
+++ scripts/dragAndDrop.js
@@ -0,0 +1,8 @@
+function addDnDHandlers() {
+
+    var coffeeimages = document.getElementByClassName("productarticlewide");
+    var shoppingCartDragzone = document.getElementByID("shoppingcart");
+    var shoppingcart = document.querySelectorAll("#shoppingcart ul")[0];
+
+
+}
